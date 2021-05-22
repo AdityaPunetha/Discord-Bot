519c324daf5e196ce80b704ffb9c206a3c420cd2
AdityaPunetha <adityapunetha6@gmail.com>
2021-05-22 10:50:25 +0530

    added quote feature

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import discord
+import os
+import requests
+import json
 
 client = discord.Client()
 TOKEN = 'ODQ1NTEyOTA1MDUwNDIzMzE3.YKiDQQ.hM6w3ih0jwSew_WhC_2vJYs0d3c'
+
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+    return quote
 
 
 @client.event
@@ -15,6 +25,8 @@ async def on_message(message):
         return
     if message.content.startswith('$hello'):
         await message.channel.send("Hello!")
+    if message.content.startswith('$inspire'):
+        await message.channel.send(get_quote())
 
 
 client.run(TOKEN)
