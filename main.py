c32297e8ff394442d1f912745e3ef88a7aaedc3a
AdityaPunetha <adityapunetha6@gmail.com>
2021-05-22 11:20:10 +0530

    added starter encouragements feature

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+'''REPLACE TOKEN WITH os.environ['TK'] REPLACE TOKEN WITH os.environ['TK'] REPLACE TOKEN WITH os.environ['TK'] '''
+
 TOKEN = 'ODQ1NTEyOTA1MDUwNDIzMzE3.YKiDQQ.hM6w3ih0jwSew_WhC_2vJYs0d3c'
+
+'''REPLACE TOKEN WITH os.environ['TK'] REPLACE TOKEN WITH os.environ['TK'] REPLACE TOKEN WITH os.environ['TK'] '''
+
+sad_words = ["sad", "depressed", "unhappy", 'angry', 'miserable', 'depressing']
+starter_encouragements = ['Cheer up!', 'Hang in there']
 
 
 def get_quote():
@@ -23,10 +32,13 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    msg = message.content
     if message.content.startswith('$hello'):
         await message.channel.send("Hello!")
     if message.content.startswith('$inspire'):
         await message.channel.send(get_quote())
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
 
 
 client.run(TOKEN)
